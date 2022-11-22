768b1ded5e3ba09c6dc676779c84a99e609c35a9
Ashary Vermaysha <asharyver13@gmail.com>
2022-11-22 11:00:22 +0700

    refactor user model

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -22,7 +22,7 @@ export default class User extends BaseModel {
   public updatedAt: DateTime
 
   @beforeSave()
-  public static async hashPassword (user: User) {
+  public static async hashPassword(user: User) {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password)
     }
