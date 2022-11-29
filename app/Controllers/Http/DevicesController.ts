526cb1b4c4c89727441969bdd46ebcf3fc742644
Ashary Vermaysha <asharyver13@gmail.com>
2022-11-29 18:56:13 +0700

    delete devices

--- app/Controllers/Http/DevicesController.ts
+++ app/Controllers/Http/DevicesController.ts
@@ -5,6 +5,10 @@ import Contact from 'App/Models/Contact'
 import Message from 'App/Models/Message'
 import Group from 'App/Models/Group'
 import StoreDeviceValidator from 'App/Validators/Device/StoreDeviceValidator'
+import { Whatsapp } from 'App/Services/Whatsapp'
+import Drive from '@ioc:Adonis/Core/Drive'
+import MessageMedia from 'App/Models/MessageMedia'
+import Chat from 'App/Models/Chat'
 
 export default class DevicesController {
   /**
@@ -149,7 +153,46 @@ export default class DevicesController {
     }
 
     try {
+      // Delete message media
+      const messageMedias = await MessageMedia.query()
+        .select('file_path')
+        .whereNotNull('file_path')
+        .orWhereNot('file_path', '')
+      for (const media of messageMedias) {
+        Drive.delete(media.filePath!)
+      }
+
+      // Delete chat photo profile
+      const chats = await Chat.query()
+        .select('photo_profile')
+        .whereNotNull('photo_profile')
+        .orWhereNot('photo_profile', '')
+      for (const chat of chats) {
+        Drive.delete(chat.photoProfile!)
+      }
+
+      // Delete contact photo profile
+      const contacts = await Contact.query()
+        .select('img_url')
+        .whereNotNull('img_url')
+        .orWhereNot('img_url', '')
+      for (const contact of contacts) {
+        Drive.delete(contact.imgUrl!)
+      }
+
+      // Delete group photo profile
+      const groups = await Group.query()
+        .select('photo_profile')
+        .whereNotNull('photo_profile')
+        .orWhereNot('photo_profile', '')
+      for (const group of groups) {
+        Drive.delete(group.photoProfile!)
+      }
+
+      Whatsapp.logout(device.id)
+
       await device.delete()
+
       return response.ok({
         message: 'Device data has been deleted',
       })
