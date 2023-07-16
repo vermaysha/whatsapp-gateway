735fe6d7b54c0d20cadfe8b51b34cf700332cc6c
Ashary Vermaysha <vermaysha@gmail.com>
2023-07-16 19:30:39 +0700

    feat(web): react components

---
+++ apps/web/src/components/Navigation.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import Link from "next/link"
+
+export interface INavLink {
+  icon: React.ReactNode
+  href: string
+  name: string
+  newTab?: boolean
+}
+
+export interface INavigation {
+  navLinks: INavLink[]
+}
+
+export function Navigation({ navLinks }: INavigation) {
+  const pathname = usePathname()
+
+  return (
+    <>
+      {navLinks.map((link, i) => {
+        const isActive = pathname.startsWith(link.href)
+        return (
+          <li key={i} className="mb-2">
+            <Link href={link.href} className={isActive ? 'active' : ''} target={link.newTab ? '_blank' : '_self'}>
+              <span>
+                {link.icon}
+              </span>
+              <span className="capitalize">{link.name}</span>
+            </Link>
+          </li>
+        )
+      })}
+    </>
+  )
+}
---
+++ apps/web/src/components/Sidebar.tsx
@@ -0,0 +1,150 @@
+import { INavLink, Navigation } from "./Navigation"
+
+const mainNav: INavLink[] = [
+  {
+    name: "Dashboard",
+    href: "/",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Device",
+    href: "/device",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Message",
+    href: "/message",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z"
+        />
+      </svg>
+    ),
+  },
+]
+
+const apiNav: INavLink[] = [
+  {
+    name: "API Token",
+    href: "/api-token",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "API Documentation",
+    href: "/api-docs",
+    newTab: true,
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
+        />
+      </svg>
+    ),
+  },
+]
+
+function Sidebar() {
+  return (
+    <aside className="h-screen">
+      <div className="border-b z-20 bg-base-100 sticky top-0  hidden items-center px-4 py-2  lg:flex ">
+        <a href="/" className="flex-0 btn btn-ghost px-2">
+          <div className="font-title inline-flex text-lg md:text-2xl">
+            <span className="uppercase">WA</span>{" "}
+            <span className="capitalize ">Gateway</span>
+          </div>
+        </a>
+      </div>
+      <ul className="menu w-80 h-screen bg-base-100 lg:border-r">
+        {<Navigation navLinks={mainNav} />}
+        <li className="menu-title">API</li>
+        {<Navigation navLinks={apiNav} />}
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
