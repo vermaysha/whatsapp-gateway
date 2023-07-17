ce6b8bb31ccbc2553adef04dfb8b1a9ceb19eb41
Ashary Vermaysha <vermaysha@gmail.com>
2023-07-17 07:35:52 +0700

    feat: 404 page not found

---
+++ apps/web/src/app/not-found.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+
+export const metadata: Metadata = {
+  title: "404 Not Found | Whatsapp Gateway",
+}
+
+export default function NotFound() {
+  return (
+    <div className="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+      <div className="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+        <div className="relative">
+          <div className="absolute">
+            <div className="">
+              <h1 className="my-2 text-gray-800 font-bold text-2xl">
+                Looks like you&apos;ve found the doorway to the great nothing
+              </h1>
+              <p className="my-2 text-gray-800">
+                Sorry about that! Please visit our hompage to get where you need
+                to go.
+              </p>
+              <Link href={"/"} className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50">
+                Take me there!
+              </Link>
+            </div>
+          </div>
+          <div>
+            <Image
+              src="/404-2.png"
+              alt="404 Page"
+              width={400}
+              height={400}
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <Image src="/Group.png" alt="group" width={400} height={400} />
+      </div>
+    </div>
+  )
+}
