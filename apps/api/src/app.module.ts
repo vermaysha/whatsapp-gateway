4f4662a9fc2c595e1cb455dc472cac42754d663a
Ashary Vermaysha <vermaysha@gmail.com>
2023-07-18 11:58:09 +0700

    refactor: code style

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
+import { DevicesModule } from './devices/devices.module'
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, DevicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
