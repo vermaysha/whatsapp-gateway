67fb6a1b4982602dec2a113bbb3b6c9c111d03a2
Ashary Vermaysha <vermaysha@gmail.com>
2023-07-24 18:24:35 +0700

    refactor: fix route prefix

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -21,7 +21,7 @@ export default function NotFound() {
                 to go.
               </p>
               <Link
-                href={"/"}
+                href={"/dashboard"}
                 className="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-opacity-50"
               >
                 Take me there!
