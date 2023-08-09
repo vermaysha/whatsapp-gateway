3080d7180d6ab9afb7c9febf79b43eb9efe44972
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-09 21:38:42 +0700

    chore: disable FastifyRequest extends

--- apps/api/src/env.d.ts
+++ apps/api/src/env.d.ts
@@ -1,15 +1,15 @@
 import { FastifyRequest } from 'fastify'
 
 declare module 'fastify' {
-  interface FastifyRequest {
-    user: {
-      uuid: string
-      username: string
-      iat: number
-      exp: number
-    } | null
-    // tambahkan properti atau metode lainnya jika diperlukan
-  }
+  // interface FastifyRequest {
+  //   user: {
+  //     uuid: string
+  //     username: string
+  //     iat: number
+  //     exp: number
+  //   } | null
+  //   // tambahkan properti atau metode lainnya jika diperlukan
+  // }
 
   // Extends Fastify Session Key
   interface Session {
