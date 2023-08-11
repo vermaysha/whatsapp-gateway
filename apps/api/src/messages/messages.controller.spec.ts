dda4b7bb7297aaf20ac04a70169d0579c13ae40b
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-11 09:43:57 +0700

    test: import module in messages testing

--- apps/api/src/messages/messages.controller.spec.ts
+++ apps/api/src/messages/messages.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { MessagesController } from './messages.controller'
+import { MessagesModule } from './messages.module'
 
 describe('MessagesController', () => {
   let controller: MessagesController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [MessagesModule],
       controllers: [MessagesController],
     }).compile()
 
