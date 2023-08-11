bfcf262e6eac85f8d2cd3f9f3298effe217924fc
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-11 10:23:09 +0700

    fix: findOne parameter changed to username

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -4,17 +4,17 @@ import { type User, prisma } from 'database'
 @Injectable()
 export class UsersService {
   /**
-   * Retrieves a user from the database based on the provided id.
+   * Retrieves a user from the database based on the provided username.
    *
-   * @param {string} id - The id of the user to be retrieved.
+   * @param {string} username - The username of the user to be retrieved.
    * @return {Promise<User | null>} - A promise that resolves to the user object, or null if no user is found.
    */
-  async findOne(id?: string | null): Promise<User | null> {
-    if (!id) return null
+  async findOne(username?: string | null): Promise<User | null> {
+    if (!username) return null
 
     return await prisma.user.findFirst({
       where: {
-        id,
+        username,
       },
     })
   }
