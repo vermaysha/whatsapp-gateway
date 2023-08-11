f232c5a15a21e9f716184ba829853a460512a3f2
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-11 09:46:16 +0700

    chore: write DTO Validation Comment

--- apps/api/src/auth/auth.dto.ts
+++ apps/api/src/auth/auth.dto.ts
@@ -1,5 +1,16 @@
 export class SignInDto {
+  /**
+   * The username of the user to sign in.
+   *
+   * @pattern ^[a-zA-Z0-9\-_.]+$
+   * @minLength 4
+   */
   username: string
 
+  /**
+   * The password of the user to sign in.
+   *
+   * @minLength 8
+   */
   password: string
 }
--- apps/api/src/chats/chats.dto.ts
+++ apps/api/src/chats/chats.dto.ts
@@ -1,4 +1,19 @@
 export class IChatsList {
+  /**
+   * The page number to retrieve.
+   *
+   * @type int
+   * @minimum 1
+   * @default 1
+   */
   page?: number | null
+
+  /**
+   * The number of messages per page.
+   *
+   * @type int
+   * @minimum 1
+   * @default 1
+   */
   perPage?: number | null
 }
