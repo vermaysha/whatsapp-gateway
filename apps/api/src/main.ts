6a3d6bbb1f033417baad97a86c53885109c64a18
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-13 07:26:39 +0700

    fix: multiply by 1000 for maxAge

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     secure: 'auto',
     sameSite: 'lax',
     path: '/',
-    maxAge: 60 * 60 * 24 * 365, // 1years
+    maxAge: 60 * 60 * 24 * 365 * 1000, // 1years
   }
 
   if (!secret) {
