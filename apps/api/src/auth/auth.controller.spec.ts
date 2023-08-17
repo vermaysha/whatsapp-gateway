bb5655fc08b0886b5de11137e82b0ceb1f3425e8
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-17 14:54:50 +0700

    test: fix test case

--- apps/api/src/auth/auth.controller.spec.ts
+++ apps/api/src/auth/auth.controller.spec.ts
@@ -2,13 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { AuthController } from './auth.controller'
 import { UsersModule } from '../users/users.module'
 import { AuthModule } from './auth.module'
+import { JwtService } from '@nestjs/jwt'
+import { AuthService } from './auth.service'
+import { UsersService } from '../users/users.service'
 
 describe('AuthController', () => {
   let controller: AuthController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [UsersModule, AuthModule],
+      providers: [AuthService, UsersService, JwtService],
       controllers: [AuthController],
     }).compile()
 
