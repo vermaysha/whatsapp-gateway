bcaaf482817db2066d83f57af9a8ae3656a13d01
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-17 14:33:00 +0700

    chore: handle connect and disconnect events

--- apps/api/src/events/events.gateway.ts
+++ apps/api/src/events/events.gateway.ts
@@ -5,24 +5,47 @@ import {
   WebSocketGateway,
   WebSocketServer,
   WsResponse,
+  type OnGatewayInit,
+  type OnGatewayConnection,
+  type OnGatewayDisconnect,
 } from '@nestjs/websockets'
 import { from, Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
-import { Server } from 'socket.io'
+import type { Server, Socket } from 'socket.io'
 import { AuthWsGuard } from '../auth/auth-ws.guard'
 
-@WebSocketGateway({
-  cors: {
-    origin: '*',
-  },
-})
+@WebSocketGateway()
 @UseGuards(AuthWsGuard)
-export class EventsGateway {
+export class EventsGateway
+  implements
+    OnGatewayInit<Server>,
+    OnGatewayConnection<Socket>,
+    OnGatewayDisconnect<Socket>
+{
   @WebSocketServer()
   server: Server
+  connectedUsers: number = 1
+
+  afterInit(server: Server) {
+    server.emit('events', { message: 'Gateway initialized' })
+    console.log('Gateway initialized')
+  }
+
+  handleConnection(socket: Socket) {
+    socket.emit('events', { message: 'Connection established', id: socket.id })
+    console.log('Connection established', socket.id)
+    this.connectedUsers++
+  }
+
+  handleDisconnect(client: Socket) {
+    client.emit('events', { message: 'Disconnected', id: client.id })
+    console.log('Disconnected', client.id)
+    this.connectedUsers--
+  }
 
   @SubscribeMessage('events')
   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    console.log('events', data)
     return from([1, 2, 3]).pipe(
       map((item) => ({ event: 'events', data: item })),
     )
