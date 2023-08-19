fd6d98d4f55761e3d5c1ea1968c78e5e1e7f60ee
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-19 22:27:43 +0700

    feat: new loading components

---
+++ apps/web/components/Loading.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <span class="sr-only">Loading</span>
+    <div class="flex items-center justify-center gap-4">
+      <span className="loading loading-bars loading-xs text-info"></span>
+      <span className="loading loading-bars loading-sm text-warning"></span>
+      <span className="loading loading-bars loading-md text-success"></span>
+      <span className="loading loading-bars loading-lg text-error"></span>
+    </div>
+  </div>
+</template>
