67351e512cc79e843be3ca003a5fbc04720ad402
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-26 21:27:31 +0700

    fix: import configService in test case

--- apps/api/src/events/events.gateway.spec.ts
+++ apps/api/src/events/events.gateway.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { EventsGateway } from './events.gateway'
 import { AuthWsGuard } from '../auth/auth-ws.guard'
 import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
 
 describe('EventsGateway', () => {
   let gateway: EventsGateway
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthWsGuard, EventsGateway, JwtService],
+      providers: [AuthWsGuard, EventsGateway, JwtService, ConfigService],
     }).compile()
 
     gateway = module.get<EventsGateway>(EventsGateway)
