9f1a30091c3b57fa310fb4ee7aca7b36bcf2b013
Ashary Vermaysha <vermaysha@gmail.com>
2023-08-27 08:54:16 +0700

    chore: add typia comment docs

--- apps/api/src/api-token/api-token.dto.ts
+++ apps/api/src/api-token/api-token.dto.ts
@@ -38,9 +38,20 @@ export class ListDTO {
 }
 
 export class ApiTokenCreateDto {
+  /**
+   * The name of the API token.
+   */
   name: string
 
+  /**
+   * The description of the API token.
+   */
   description?: string | null
 
-  expiredAt?: Date | null
+  /**
+   * The expiration date of the API token.
+   *
+   * @format date-time
+   */
+  expiredAt?: string | null
 }
