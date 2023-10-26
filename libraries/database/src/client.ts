00677bb26fee8b7b21ab6ac267666aba56784598
Ashary Vermaysha <vermaysha@gmail.com>
2023-10-26 08:34:36 +0700

    chore: re-export @prisma/client

--- libraries/database/src/client.ts
+++ libraries/database/src/client.ts
@@ -1,10 +1,10 @@
-/* eslint-disable no-var */
+/* eslint-disable */
 import { PrismaClient } from '@prisma/client'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = global.prisma || new PrismaClient()
+export const prisma: PrismaClient = global.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
--- libraries/database/src/index.ts
+++ libraries/database/src/index.ts
@@ -1,2 +1,4 @@
-export * from './client'
 export * from '@prisma/client'
+export * from './client'
+
+prisma?.$connect()
