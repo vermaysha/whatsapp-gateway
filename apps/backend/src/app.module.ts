c42dc0c2f9f5f8c1127397452c9c766d1af046c1
Ashary Vermaysha <vermaysha@gmail.com>
2023-10-31 14:25:00 +0700

    feat: serve static file and allow cors for all

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -5,9 +5,19 @@ import { WhatsappModule } from './whatsapp/whatsapp.module';
 import { MessageModule } from './message/message.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { EventModule } from './event/event.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), WhatsappModule, MessageModule, EventModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    EventEmitterModule.forRoot(),
+    WhatsappModule,
+    MessageModule,
+    EventModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+  });
+
   app.enableShutdownHooks();
+
+  app.setGlobalPrefix('api');
 
   await app.listen(3000);
 }
