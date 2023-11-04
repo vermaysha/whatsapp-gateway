3eb0093ef7bdc16a3fa8edf6f3e380d8c44449a7
Ashary Vermaysha <vermaysha@gmail.com>
2023-11-04 14:37:03 +0700

    feat: install & setup pinia

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import '@tabler/core/dist/css/tabler.min.css'
 import './style.css'
 import 'bootstrap'
@@ -6,6 +7,8 @@ import App from './App.vue'
 
 import { router } from './router'
 
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
