4423011758dca1ccacb85fa7a5334221a965856c
Ashary Vermaysha <vermaysha@gmail.com>
2023-11-07 14:09:39 +0700

    chore: add screenshot

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
   <h1>Whatsapp Gateway</h1>
   <small>(Under Development)</small>
 </div>
+
+## Preview
+![Homepage](./screenshots/home.jpg "Homepage")
--- apps/frontend/src/layouts/Main.vue
+++ apps/frontend/src/layouts/Main.vue
@@ -2,49 +2,15 @@
   <div class="page">
     <header class="navbar navbar-expand-sm navbar-light d-print-none">
       <div class="container-xl">
-        <h1
+        <RouterLink
+          :to="{name: 'Home'}"
           class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3"
         >
-          <a href="#">
-            <img
-              src="https://preview.tabler.io/static/logo.svg"
-              width="110"
-              height="32"
-              alt="Tabler"
-              class="navbar-brand-image"
-            />
-          </a>
-        </h1>
+        WAServer
+        </RouterLink>
 
         <div class="navbar-nav flex-row order-md-last">
           <div class="d-none d-md-flex">
-            <a
-              href="?theme=dark"
-              class="nav-link px-0 hide-theme-dark"
-              data-bs-toggle="tooltip"
-              data-bs-placement="bottom"
-              aria-label="Enable dark mode"
-              data-bs-original-title="Enable dark mode"
-            >
-              <!-- Download SVG icon from http://tabler-icons.io/i/moon -->
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="icon"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path
-                  d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"
-                ></path>
-              </svg>
-            </a>
             <a
               href="?theme=light"
               class="nav-link px-0 hide-theme-light"
@@ -53,25 +19,7 @@
               aria-label="Enable light mode"
               data-bs-original-title="Enable light mode"
             >
-              <!-- Download SVG icon from http://tabler-icons.io/i/sun -->
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="icon"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                stroke-width="2"
-                stroke="currentColor"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
-                <path
-                  d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7"
-                ></path>
-              </svg>
+              <IconSun />
             </a>
             <div class="nav-item dropdown d-none d-md-flex me-3">
               <a
@@ -82,25 +30,7 @@
                 aria-label="Show notifications"
                 aria-expanded="true"
               >
-                <!-- Download SVG icon from http://tabler-icons.io/i/bell -->
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="icon"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  stroke-width="2"
-                  stroke="currentColor"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path
-                    d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"
-                  ></path>
-                  <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
-                </svg>
+                <IconBell/>
                 <span class="badge bg-red"></span>
               </a>
               <div
@@ -130,28 +60,7 @@
                         </div>
                         <div class="col-auto">
                           <a href="#" class="list-group-item-actions">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="icon text-muted"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              stroke-width="2"
-                              stroke="currentColor"
-                              fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path
-                                stroke="none"
-                                d="M0 0h24v24H0z"
-                                fill="none"
-                              ></path>
-                              <path
-                                d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
-                              ></path>
-                            </svg>
+                            <IconStar/>
                           </a>
                         </div>
                       </div>
@@ -172,28 +81,7 @@
                         </div>
                         <div class="col-auto">
                           <a href="#" class="list-group-item-actions show">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="icon text-yellow"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              stroke-width="2"
-                              stroke="currentColor"
-                              fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path
-                                stroke="none"
-                                d="M0 0h24v24H0z"
-                                fill="none"
-                              ></path>
-                              <path
-                                d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
-                              ></path>
-                            </svg>
+                            <IconStar/>
                           </a>
                         </div>
                       </div>
@@ -213,28 +101,7 @@
                         </div>
                         <div class="col-auto">
                           <a href="#" class="list-group-item-actions">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="icon text-muted"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              stroke-width="2"
-                              stroke="currentColor"
-                              fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path
-                                stroke="none"
-                                d="M0 0h24v24H0z"
-                                fill="none"
-                              ></path>
-                              <path
-                                d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
-                              ></path>
-                            </svg>
+                            <IconStar/>
                           </a>
                         </div>
                       </div>
@@ -256,28 +123,7 @@
                         </div>
                         <div class="col-auto">
                           <a href="#" class="list-group-item-actions">
-                            <!-- Download SVG icon from http://tabler-icons.io/i/star -->
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="icon text-muted"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              stroke-width="2"
-                              stroke="currentColor"
-                              fill="none"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            >
-                              <path
-                                stroke="none"
-                                d="M0 0h24v24H0z"
-                                fill="none"
-                              ></path>
-                              <path
-                                d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
-                              ></path>
-                            </svg>
+                            <IconStar/>
                           </a>
                         </div>
                       </div>
@@ -368,5 +214,5 @@
 </template>
 
 <script setup lang="ts">
-import { IconHome } from "@tabler/icons-vue";
+import { IconHome, IconSun, IconBell, IconStar } from "@tabler/icons-vue";
 </script>
